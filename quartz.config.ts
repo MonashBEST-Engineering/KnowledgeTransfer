062ca645c6c8a5c4a35c346dc2cb9644b037f459
MonashBEST <156616282+MonashBEST-Engineering@users.noreply.github.com>
2025-09-25 16:24:58 +1000

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-  baseUrl: "quartz.jzhao.xyz",
+  baseUrl: "https://monashbest-engineering.github.io/KnowledgeTransfer/",
   ignorePatterns: ["private", "templates", ".obsidian"],
   defaultDateType: "modified",
   output: "public",  // <--- Add this line here
